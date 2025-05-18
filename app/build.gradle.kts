859e674a496ad5f341f9d1a44e8a2bc751b45f07
isDohyeon <eun041212@gmail.com>
2025-05-19 01:43:30 +0900

    feat(talk) : 커뮤니티에 사진을 업로드하면 Firebase Storage에 저장하는 기능 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-storage")
     // glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/hnu/multimedia/sololifetalk/ui/talk/TalkWriteActivity.kt
+++ app/src/main/java/hnu/multimedia/sololifetalk/ui/talk/TalkWriteActivity.kt
@@ -1,9 +1,12 @@
 package hnu.multimedia.sololifetalk.ui.talk
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import hnu.multimedia.sololifetalk.databinding.ActivityTalkWriteBinding
 import hnu.multimedia.sololifetalk.util.FirebaseRef
 import hnu.multimedia.sololifetalk.util.MyUtils
@@ -11,6 +14,7 @@ import hnu.multimedia.sololifetalk.util.MyUtils
 class TalkWriteActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityTalkWriteBinding.inflate(layoutInflater) }
+    private var uri = Uri.EMPTY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,23 @@ class TalkWriteActivity : AppCompatActivity() {
             val title = binding.editTextTitle.text.toString()
             val content = binding.editTextContent.text.toString()
             val uid = Firebase.auth.currentUser?.uid.toString()
-            FirebaseRef.talks.push().setValue(TalkModel(title, content, uid, MyUtils.getCurrentDateTime()))
+            val pushRef = FirebaseRef.talks.push()
+            FirebaseRef.talks.push()
+                .setValue(TalkModel(title, content, uid, MyUtils.getCurrentDateTime()))
+            Firebase.storage.reference.child("${pushRef.key.toString()}.jpg").putFile(uri)
             finish()
+        }
+
+        val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            _uri ->
+            _uri.let {
+                uri = _uri
+                binding.imageViewPhoto.setImageURI(uri)
+            }
+        }
+
+        binding.imageViewPhoto.setOnClickListener {
+            launcher.launch("image/*")
         }
     }
 }
